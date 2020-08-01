45cbed495d4b7a2f17f1adb660bc3ea166ea7553
Anna <ms.annabell29@gmail.com>
2020-08-01 18:05:04 +0300

    initial commit

---
+++ collectionViewHW/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  collectionViewHW
+//
+//  Created by Анна on 01.08.2020.
+//  Copyright © 2020 anna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
